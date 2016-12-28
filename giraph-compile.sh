4d1f501a7a1d1bd9a5d51c7b2c8c6989fac084ca
ps <piotrekskweres@gmail.com>
2016-12-28 18:02:53 +0100

    empty build directory before compile

--- giraph-compile.sh
+++ giraph-compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf build
+mkdir build
 javac -Xlint:unchecked -cp $GIRAPH_HOME/giraph-core/target/giraph-1.2.0-hadoop2-for-hadoop-2.5.1-jar-with-dependencies.jar:$($HADOOP_HOME/bin/hadoop classpath) -d build/ src/pdd/*.java
 
 cp giraph.jar ./gol.jar
